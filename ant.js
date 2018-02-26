f8ec55720831c101f35b9a38f0b93ff7e03e2c16
Peter Coles <peter@mrcoles.com>
2018-02-26 18:08:46 -0500

    making it in canvas!

--- ant.js
+++ ant.js
@@ -28,3 +28,54 @@ const initGame = () => {
     grid: grid
   };
 };
+
+const step = game => {
+  let { grid, ant } = game;
+  grid = _shallowCopy(grid);
+  ant = _shallowCopy(ant);
+  let color = grid[ant.row][ant.col];
+  ant.dir = (ant.dir + (color === WHITE ? 1 : -1)) % 4;
+  grid[ant.row][ant.col] = color === WHITE ? BLACK : WHITE;
+
+  let row_max = grid.length - 1;
+  let col_max = grid[0].length - 1;
+
+  switch (ant.dir) {
+    case DIR_UP:
+      ant.row = Math.max(0, ant.row - 1);
+      break;
+    case DIR_DOWN:
+      ant.row = Math.min(row_max, ant.row + 1);
+      break;
+    case DIR_LEFT:
+      ant.col = Math.max(0, ant.col - 1);
+      break;
+    case DIR_RIGHT:
+      ant.col = Math.min(col_max, ant.col + 1);
+      break;
+    default:
+      throw new Error(`UKNOWN DIRECTIONS: ${ant.dir}`);
+  }
+
+  return { grid, ant };
+};
+
+const _shallowCopy = obj => Object.assign({}, obj);
+
+const loop = (callback, game) => {
+  game = game || initGame();
+  setTimeout(() => {
+    callback && callback();
+    loop(callback, game);
+  }, 500);
+};
+
+window.Ant = {
+  initGame,
+  step,
+  loop
+};
+
+// main runner
+
+// require.main === module && loop();
---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("APP!", window.step);
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+let game = Ant.initGame();
